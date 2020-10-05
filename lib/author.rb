84c5549555c0f082a56f5a8fc942d60fa42cdc70
lisellelogan <liselle.logan@yahoo.com>
2020-10-04 20:17:58 -0400

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,21 @@
+class Author
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def add_post(post)
+        post.author = self
+    end
+
+    def posts
+        Post.all.select {|post| post.author == self}
+    end
+
+    def add_post_by_title(title)
+        posts = Post.new(title)
+        add_post(title)
+    end
+end
